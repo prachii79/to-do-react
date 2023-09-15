02f2a039102c2405f35625b68410cf23722b7eba
Prachi Vaishya <vaishyaprachi14@gmail.com>
2023-09-15 07:56:23 +0530

    trying to fix edit feature

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,55 @@ import { ModalTask } from "./interfaces";
 import CreateModal from "./components/CreateModal";
 import EditModal from "./components/EditModal";
 import NewTask from "./components/NewTask";
-import { Accordion } from 'react-accessible-accordion';
+import { Accordion } from "react-accessible-accordion";
 
 export default function App() {
-
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  function openModal() { setIsOpen(true) }
-  function closeModal() { setIsOpen(false) }
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const [editModalIsOpen, setEditIsOpen] = React.useState(false);
-  function openEditModal() { setEditIsOpen(true) }
-  function closeEditModal() { setEditIsOpen(false) }
+  function openEditModal() {
+    setEditIsOpen(true);
+  }
+  function closeEditModal() {
+    setEditIsOpen(false);
+  }
 
   const [date, setDate] = useState("");
   const [prior, setPrior] = useState("");
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
-  //const [done, setDone] = useState(false)
   const [tasks, setTasks] = useState(() => {
     const storedTask = JSON.parse(localStorage.getItem("data"));
     return storedTask || [];
   });
-  
+
   const [editPrior, setEditPrior] = useState("");
   const [editTitle, setEditTitle] = useState("");
   const [editDetails, setEditDetails] = useState("");
+  const [editDate, setEditDate] = useState("")
 
   React.useEffect(() => {
     localStorage.setItem("data", JSON.stringify(tasks));
   }, [tasks]);
 
   function handleDone(id: string) {
-    console.log("clicked")
     setTasks((prevTasks: ModalTask[]) => {
-      return prevTasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      );
+      return prevTasks.map((task) => {
+        if (task.id === id) {
+          //console.log("task:", task)
+          return { ...task, done: !task.done };
+        }
+        return task;
+      });
     });
   }
-  
+
   //const checked = done ? "/images/checked2.svg" : "/images/unchecked.svg"
 
   function handleCreateTasks() {
@@ -51,9 +61,9 @@ export default function App() {
         id: nanoid(),
         date: date,
         prior: prior,
-        title:title,
-        details:details,
-        done:false
+        title: title,
+        details: details,
+        done: false,
       };
       return [...prevTasks, task];
     });
@@ -61,15 +71,15 @@ export default function App() {
     setTitle("");
     setDetails("");
 
-    if((prior || title || details) === null ){
-      alert("fill the necessary fields")
-    } else{
+    if ((prior || title || details) === null) {
+      alert("fill the necessary fields");
+    } else {
       closeModal();
     }
   }
 
   function deleteTask(id: string, e) {
-    e.stopPropagation()
+    e.stopPropagation();
     setTasks((prevTasks: ModalTask[]) => {
       return prevTasks.filter((task) => task.id != id);
     });
@@ -79,34 +89,45 @@ export default function App() {
   function handleEdit(id: string) {
     setTasks((prevTasks: ModalTask[]) => {
       return prevTasks.map((task) =>
-        task.id === id ? { ...task, prior: editPrior, title: editTitle, details: editDetails } : task
+        task.id === id
+          ? {
+              ...task,
+              prior: editPrior,
+              title: editTitle,
+              details: editDetails,
+              date: editDate
+            }
+          : task
       );
     });
     setEditPrior("");
     setEditTitle("");
     setEditDetails("");
     closeEditModal();
-  }  
+  }
 
-  function editTask(id: string, e){
-    e.stopPropagation()
-    setEditIsOpen(true)
-    const editable = tasks.find((taask: ModalTask) => taask.id === id)
+  function editTask(id: string, e) {
+    e.stopPropagation();
+    setEditIsOpen(true);
+    const editable = tasks.find((taask: ModalTask) => taask.id === id);
 
     if (editable) {
       setEditPrior(editable.prior);
       setEditTitle(editable.title);
       setEditDetails(editable.details);
-    }  
+      setEditDate(editable.date)
+    }
+
+    //return editable
   }
 
-  const taskToEdit = tasks.find((task: ModalTask) => task.title === editTitle)
+  const taskToEdit = tasks.find((task: ModalTask) => task.title === editTitle);
 
   const modalTasks = tasks.map((taask: ModalTask) => {
-    return(
-      <NewTask 
-        key = {taask.id}
-        id = {taask.id}
+    return (
+      <NewTask
+        key={taask.id}
+        id={taask.id}
         priority={taask.prior}
         tit={taask.title}
         det={taask.details}
@@ -116,69 +137,104 @@ export default function App() {
         done={taask.done}
         handle={handleDone}
       />
-    )
-  })  
+    );
+  });
 
   let today = new Date();
-  let dd = String(today.getDate()).padStart(2, '0');
-  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   let yyyy = today.getFullYear();
-  let d = today.getDay()
-  const months = ["January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December"]
-  const month = months[parseInt(mm) - 1]
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-  const day = days[parseInt(d) - 1]
+  let d = today.getDay();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = months[parseInt(mm) - 1];
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const day = days[parseInt(d) - 1];
   return (
     <div className="w-5/12 border-2 rounded-md flex flex-col items-center justify-center mx-auto p-3">
       <header className="flex justify-between w-full h-22 border-b-2 border-dotted pb-2">
         <div className="flex items-center">
           <div className="text-4xl font-bold">{dd}</div>
           <div className="ml-2">
-            <p className="text-md font-semibold">{month} {yyyy}</p>
+            <p className="text-md font-semibold">
+              {month} {yyyy}
+            </p>
             <p className="text-sm">{day}</p>
           </div>
         </div>
 
         <div className="flex items-center">
-          <img src="./images/create.svg" alt="" className="h-9 w-9"  onClick={openModal}/>
+          <img
+            src="./images/create.svg"
+            alt=""
+            className="h-9 w-9"
+            onClick={openModal}
+          />
           <p className="font-bold text-xs">CREATE TASK</p>
         </div>
       </header>
 
-      <CreateModal 
+      <CreateModal
         open={modalIsOpen}
         setOpen={setIsOpen}
         onOpen={openModal}
         onClose={closeModal}
         prior={prior}
-        onPrior={e=>{setPrior(e.target.value)}}
+        onPrior={(e) => {
+          setPrior(e.target.value);
+        }}
         title={title}
         details={details}
-        onTitle={e=>{setTitle(e.target.value)}}
-        onDetail={e => {setDetails(e.target.value)}}
-        onDate={e => {setDate(e.target.value)}}
+        onTitle={(e) => {
+          setTitle(e.target.value);
+        }}
+        onDetail={(e) => {
+          setDetails(e.target.value);
+        }}
+        onDate={(e) => {
+          setDate(e.target.value);
+        }}
         onCreateTasks={handleCreateTasks}
       />
 
-      {editModalIsOpen && 
-          <EditModal 
+      {editModalIsOpen && (
+        <EditModal
+          id={taskToEdit.id}
           open={editModalIsOpen}
           setOpen={setEditIsOpen}
           onOpen={openEditModal}
           onClose={closeEditModal}
           prior={editPrior}
-          onPrior={e => {setEditPrior(e.target.value)}}
+          onPrior={(e) => {setEditPrior(e.target.value)}}
           title={editTitle}
           details={editDetails}
-          onTitle={e => {setEditTitle(e.target.value)}}
-          onDetail={e => {setEditDetails(e.target.value)}}
-          onEditTask={() => handleEdit(taskToEdit.id)}
-      />}
-           
-      <Accordion allowZeroExpanded>
-        {modalTasks}
-      </Accordion>
+          onTitle={(e) => {setEditTitle(e.target.value)}}
+          onDetail={(e) => {setEditDetails(e.target.value)}}
+          onEditTask={handleEdit}
+        />
+      )}
 
+      <Accordion allowZeroExpanded>{modalTasks}</Accordion>
     </div>
   );
 }
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,58 +1,82 @@
 import {
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-  } from 'react-accessible-accordion';
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
-export default function NewTask({id, priority, tit, det, delet, edit, date, done, handle}){
-    console.log("props:", { id, priority, tit, det, date, done, handle });
+export default function NewTask({
+  id,
+  priority,
+  tit,
+  det,
+  delet,
+  edit,
+  date,
+  done,
+  handle,
+}) {
+  const styles = {
+    //backgroundColor: priority === "LOW" ? "yellow" : "red",
+    backgroundColor: done ? "green" : priority === "LOW" ? "yellow" : "red"
+  };
 
-    const styles = {
-        backgroundColor: priority === "LOW" ? "yellow" : "red"
-        //backgroundColor: priority === "LOW" ? "yellow" : done ? "green" : "red"
-      }
-    //const checked = done ? "/images/checked2.svg" : "/images/unchecked.svg"
-    console.log(done)
-    //console.log(checked)
-    
-    function handleImageClick(e) {
-        e.stopPropagation()
-        console.log("Image clicked");
-        handle(id, e)
-        console.log(handle(id, e))
-      }
+  function handleImageClick(e) {
+    e.stopPropagation();
+    handle(id, e);
+  }
 
-    return(
-        <div>
-            <div>
-                <AccordionItem className="flex flex-col w-30 rounded bg-yellow-100 p-3" style={styles}>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        <div className='w-52'>
-                            <p> Due: {date}</p>
-                            <div className='flex items-center'>
-                                <div >
-                                    <img src={done ? "/images/checked2.svg" : "/images/unchecked.svg"} onClick={handleImageClick} className='w-6 h-6' />
-                                </div>
-                                <div className='flex justify-between w-full'>
-                                    <div className="font-bold text-xl">{tit}</div>
-                                    <div className='flex  mr-0'>
-                                        <img src="/images/delete.svg" onClick={(e) => {delet(id, e)}} className='w-8 h-8'/>
-                                        <img src="/images/edit.svg" onClick={(e) => {edit(id, e)}} className='w-8 h-8'/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                    <div className='bg-pink-100'>
-                        <p>{det}</p>
+  return (
+    <div>
+      <div>
+        <AccordionItem
+          className="flex flex-col w-30 rounded bg-yellow-100 p-3"
+          style={styles}
+        >
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              <div className="w-52">
+                <p> Due: {date}</p>
+                <div className="flex items-center">
+                  <div>
+                    <img
+                      src={
+                        done ? "/images/checked2.svg" : "/images/unchecked.svg"
+                      }
+                      onClick={handleImageClick}
+                      className="w-6 h-6"
+                    />
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <div className="font-bold text-xl">{tit}</div>
+                    <div className="flex  mr-0">
+                      <img
+                        src="/images/delete.svg"
+                        onClick={(e) => {
+                          delet(id, e);
+                        }}
+                        className="w-8 h-8"
+                      />
+                      <img
+                        src="/images/edit.svg"
+                        onClick={(e) => {
+                          edit(id, e);
+                        }}
+                        className="w-8 h-8"
+                      />
                     </div>
-                </AccordionItemPanel>
-                </AccordionItem>
+                  </div>
+                </div>
+              </div>
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <div className="bg-pink-100">
+              <p>{det}</p>
             </div>
-        </div>
-    )
+          </AccordionItemPanel>
+        </AccordionItem>
+      </div>
+    </div>
+  );
 }
